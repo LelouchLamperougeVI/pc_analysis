d6eda13c5748d0c0bcc3eeebaee56ff6a6b1d204
LelouchLamperougeVI <33172704+LelouchLamperougeVI@users.noreply.github.com>
2017-11-18 13:52:27 -0700

    Sequence alignment

---
+++ get_run_sequence.m
@@ -0,0 +1,16 @@
+function run_sequences=get_run_sequence(behavior,tcs,deconv)
+% construct sequences from running-related transients for sequence alignment
+
+[behavior,deconv]=convert_behavior(behavior,tcs,deconv);
+
+analysis=pc_batch_analysis(behavior,deconv);
+
+v2struct(analysis);
+v2struct(behavior);
+
+run_sequences=zeros(length(trials_ts)-1,size(deconv,2));
+
+for i=1:length(trials_ts)-1
+    seq=max(deconv(trials_ts(i):trials_ts(i+1)-1,:));
+    [~,run_sequences(i,:)]=sort(seq);
+end
