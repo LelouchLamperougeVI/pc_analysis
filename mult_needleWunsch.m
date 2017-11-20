65210d469f835400f45a69bbf3e168036f6a0746
LelouchLamperougeVI <33172704+LelouchLamperougeVI@users.noreply.github.com>
2017-11-19 21:37:01 -0700

    cell-based needleWunsch

--- mult_needleWunsch.m
+++ mult_needleWunsch.m
@@ -3,7 +3,7 @@ function [scores,pval]=mult_needleWunsch(sequences)
 % generates score matrix of pairwise comparisons
 %
 % Inputs
-%   sequences:  a N x M matrix with N individual sequences of length M
+%   sequences:  cell containing individual sequences
 %
 % Outputs
 %   scores: matrix of Needleman-Wunsch scores for sequence pairs
@@ -11,12 +11,12 @@ function [scores,pval]=mult_needleWunsch(sequences)
 %
 % by HaoRan Chang
 
-scores=needleWunsch(sequences(1,:),sequences(1,:));
-scores=diag(scores.*ones(1,size(sequences,1)));
+scores=needleWunsch(sequences{1},sequences{1});
+scores=diag(scores.*ones(1,length(sequences)));
 
-for i=1:size(sequences,1)
-    for j=i+1:size(sequences,1)
-        scores(i,j)=needleWunsch(sequences(i,:),sequences(j,:));
+for i=1:length(sequences)
+    for j=i+1:length(sequences)
+        scores(i,j)=needleWunsch(sequences{i},sequences{j});
     end
 end
 
@@ -24,11 +24,11 @@ scores=scores+scores'-diag(diag(scores));
 
 shuffles=1000;
 shuffled_scores=zeros(1,shuffles);
-for i=1:shuffles
-    shuffled_scores(i)=needleWunsch(sequences(1,randperm(size(sequences,2))),sequences(1,randperm(size(sequences,2))));
+for i=1:shuffles % NB randomize for more sequences
+    shuffled_scores(i)=needleWunsch(sequences{1}(randperm(length(sequences{1}))),sequences{1}(randperm(length(sequences{1}))));
 end
 
-pval=zeros(size(sequences,1));
+pval=zeros(length(sequences));
 for i=1:size(pval,1)
     for j=1:size(pval,2)
         pval(i,j)=1-sum(scores(i,j)>shuffled_scores)/shuffles;
