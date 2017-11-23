f13da90c4f449a78df04bc4f064a08110f7da0bc
LelouchLamperougeVI <33172704+LelouchLamperougeVI@users.noreply.github.com>
2017-11-23 14:49:57 -0700

    Bug fix

--- get_run_sequence.m
+++ get_run_sequence.m
@@ -1,14 +1,14 @@
 function run_sequences=get_run_sequence(behavior,tcs,deconv)
 % construct sequences from running-related transients for sequence alignment
 
-[behavior,deconv]=convert_behavior(behavior,tcs,deconv);
+% [behavior,deconv]=convert_behavior(behavior,tcs,deconv);
+% 
+% analysis=pc_batch_analysis(behavior,deconv);
+% 
+% v2struct(analysis);
+% v2struct(behavior);
 
-analysis=pc_batch_analysis(behavior,deconv);
-
-v2struct(analysis);
-v2struct(behavior);
-
-deconv=deconv(:,analysis.pc_list);
+% deconv=deconv(:,analysis.pc_list);
 
 run_sequences=zeros(length(behavior.trials_ts)-1,size(deconv,2));
 
