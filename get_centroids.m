c521416b4aac01c3c93012ed2066d30b4339de6f
LelouchLamperougeVI <33172704+LelouchLamperougeVI@users.noreply.github.com>
2017-11-24 20:28:38 -0700

    let me push

--- get_centroids.m
+++ get_centroids.m
@@ -1,11 +1,7 @@
-function centroid_mask=get_centroids(analysis,maskNeurons)
-if nargin<2
-    maskNeurons=analysis.maskNeurons;
-end
-
-centroid_mask=zeros(size(maskNeurons));
+function centroid_mask=get_centroids(analysis)
+centroid_mask=zeros(size(analysis.maskNeurons));
 for i=1:length(analysis.psth)
-    centroids=regionprops(maskNeurons==i,'centroid');
+    centroids=regionprops(analysis.maskNeurons==i,'centroid');
     centroids=cat(1,centroids.Centroid);
     centroids=round(centroids);
     centroid_mask(centroids(1),centroids(2))=i;
--- merge_planes.m
+++ merge_planes.m
@@ -3,8 +3,6 @@ function analysis=merge_planes(analysis1,analysis2)
 mimg1=analysis1.mimg;
 mimg2=analysis2.mimg;
 
-mask2=analysis2.maskNeurons;
-
 [optimizer,metric]=imregconfig('monomodal');
 trans=imregtform(mimg2,mimg1,'translation',optimizer,metric);
 reg=imwarp(mimg2,trans,'OutputView',imref2d(size(mimg1)));
@@ -12,7 +10,8 @@ reg=imwarp(mimg2,trans,'OutputView',imref2d(size(mimg1)));
 figure
 imshowpair(mimg1,reg,'scaling','joint');
 
-mask2=imwarp(mask2,trans,'OutputView',imref2d(size(mimg1)));
+analysis2.maskNeurons=imwarp(analysis2.maskNeurons,trans,'OutputView',imref2d(size(mimg1)));
 
 cent1=get_centroids(analysis1);
-cent2=get_centroids(analysis2,mask2);
+cent2=get_centroids(analysis2);
+
