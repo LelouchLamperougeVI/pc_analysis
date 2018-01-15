3acab76cf685d59be8184dd44f9c372c0887a4a9
LelouchLamperougeVI <33172704+LelouchLamperougeVI@users.noreply.github.com>
2018-01-14 23:26:01 -0700

    let me push

--- MPdist.m
+++ MPdist.m
@@ -1,11 +1,20 @@
-function e=MPdist(M,N,lamb)
+function varargout=MPdist(M,N,lamb)
 % Generate Marchenko Pastur distribution to threshold significance of
 % eigenvalues for N variables and M observations
-% Returns empirical probability for each eigenvalue in vector lamb
+% Returns theoretical upper threshold and empirical probability for each eigenvalue in vector lamb
 
 q=M/N;
-a=(1+sqrt(1/q))^2;
+if q<=1
+    warning('Threshold prediction inaccurate due to q > 1 condition not satisfied');
+end
+a=(1+sqrt(1/q))^2+N^(-2/3);
 b=(1-sqrt(1/q))^2;
 
+varargout{1}=[b a];
+
+if nargin<3 || nargout<2
+    return;
+end
+
 pdf=@(q,a,b,lamb) q.*sqrt((a-lamb).*(lamb-b))./(2*pi.*lamb);
-e=pdf(q,a,b,lamb);
+varargout{2}=pdf(q,a,b,lamb);
--- lopes_pca.m
+++ lopes_pca.m
@@ -1,7 +1,21 @@
-function lopes_pca(deconv)
+function lopes_pca(deconv,smooth)
 % PCA cell-assemblies detection methods adapted from Lopes-dos-Santos et
 % al. 2011
-
+deconv=fast_smooth(deconv,smooth);
 deconv=(deconv-mean(deconv))./std(deconv); % zscore each neuron's spike activity
 corrM=corr(deconv); % compute autocorrelation matrix
 [V,e]=eig(corrM); % get eigenvalues and vectors/PCs
+
+thres=MPdist(size(deconv,1),size(deconv,2)); % get upper/lower thresholds for eigenvalue significance
+e=sum(e); % vectorize eigenvalues matrix
+assemblies=e>thres(2); % identify PC's associated with assemblies
+numNeurons=sum(e<thres(1) | e>thres(2)); % determine number of neurons participating in assemblies
+assembly_space=V(:,assemblies); % PCs with significant eigenvalues
+interaction=assembly_space*assembly_space'; % interaction matrix
+vect_lengths=arrayfun(@(x) norm(assembly_space(x,:)),1:size(assembly_space,1));
+[~,idx]=sort(vect_lengths);
+assembly_neurons=idx(end-numNeurons+1:end); % neurons with biggest norms in assembly space
+interaction=interaction(assembly_neurons,:); % remove non-assembly neurons from interaction matrix
+interaction=interaction(:,assembly_neurons);
+diagonal=repmat(vect_lengths(assembly_neurons)',1,size(interaction,1));
+interaction=interaction./diagonal'; % final interaction matrix is the scalar projection of neurons onto each other in assembly space
