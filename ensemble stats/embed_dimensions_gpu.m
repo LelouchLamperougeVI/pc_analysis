233e4435af4abdaff2dbf108d63333cbbe891938
LelouchLamperougeVI <33172704+LelouchLamperougeVI@users.noreply.github.com>
2018-06-20 00:31:58 -0600

    daSDEFWEW

--- ensemble stats/embed_dimensions_gpu.m
+++ ensemble stats/embed_dimensions_gpu.m
@@ -4,17 +4,14 @@ function te=embed_dimensions_gpu(x,y,s,l,order,chunked)
 idx=[y+s.*(0:order) x+s.*(1:order)];
 p_joint=accumarray(chunked(:,idx),1)./l;
 
-idx=[y+s.*(1:order) x+s.*(1:order)];
-p_cond_xy=accumarray(chunked(:,idx),1)./l;
-p_cond_xy=permute(p_cond_xy,[ndims(p_cond_xy)+1 1:ndims(p_cond_xy)]); %inject singletons
+p_cond_xy=sum(p_joint);
 p_cond_xy=p_joint./p_cond_xy;
 
-idx=y+s.*(0:order);
-p_joint_yy=accumarray(chunked(:,idx),1)./l;
+for i=order+2:ndims(p_joint)
+    p_joint_yy=sum(p_joint,i);
+end
 
-idx=y+s.*(1:order);
-p_joint_y=accumarray(chunked(:,idx),1)./l;
-p_joint_y=permute(p_joint_y,[ndims(p_joint_y)+1 1:ndims(p_joint_y)]);
+p_joint_y=sum(p_joint_yy,1);
 p_cond_y=p_joint_yy./p_joint_y;
 
 te=p_joint.*log2(p_cond_xy./p_cond_y);
--- ensemble stats/get_te_chunk.m
+++ ensemble stats/get_te_chunk.m
@@ -9,16 +9,11 @@ deconv=(deconv-mean(deconv,'omitnan'))./std(deconv,'omitnan');
 edges=[-1e6 min(deconv(:)):range(deconv(:))/precision:max(deconv(:))];
 edges(end)=1e6;
 deconv(isnan(deconv))=min(min(deconv))-1;
-
-deconv=single(deconv);
-
-% te=gpuArray(zeros(size(deconv,2)));
 te=zeros(size(deconv,2));
 
-% dq=parallel.pool.DataQueue;
+dq=parallel.pool.DataQueue;
 f=waitbar(0,'performing high-dimensional transfer entropy...');
-% afterEach(dq,@updateBar);
-% N=size(deconv,2)^2;
+afterEach(dq,@updateBar);
 N=size(deconv,2);
 p=1;
 
@@ -27,51 +22,20 @@ for i=0:order
     chunked(:,1+i*size(deconv,2):(i+1)*size(deconv,2))=deconv(order+1-i:end-i,:);
 end
 
-% % chunked=gpuArray(chunked);
+% chunked=gpuArray(chunked);
+% te=gpuArray(te);
 
 chunked=discretize(chunked,edges);
 l=size(chunked,1);
 s=size(deconv,2);
-% tmp=gpuArray(zeros(1,size(deconv,2)));
-% tmp=zeros(1,size(deconv,2));
-for i=1:s
-%     tmp=zeros(1,size(deconv,2));
-%     for j=1:s
-%         tmp(j)=embed_dimensions_gpu(i,j,s,l,order,chunked);
-%         send(dq,i);
-%         updateBar;
-%     end
+parfor i=1:s
     te(i,:)=arrayfun(@(x) embed_dimensions_gpu(i,x,s,l,order,chunked), 1:s);
-%     te(i,:)=tmp;
-    updateBar;
+    send(dq,i);
 end
 
-te=gather(te);
+% te=gather(te);
 
 close(f);
-
-%     function te=embed_dimensions(x,y)
-%         % x -> y embedding
-%         
-%         idx=[y+s.*(0:order) x+s.*(1:order)];
-%         p_joint=accumarray(chunked(:,idx),1)./l;
-%         
-%         idx=[y+s.*(1:order) x+s.*(1:order)];
-%         p_cond_xy=accumarray(chunked(:,idx),1)./l;
-%         p_cond_xy=permute(p_cond_xy,[ndims(p_cond_xy)+1 1:ndims(p_cond_xy)]); %inject singletons
-%         p_cond_xy=p_joint./p_cond_xy;
-%         
-%         idx=y+s.*(0:order);
-%         p_joint_yy=accumarray(chunked(:,idx),1)./l;
-%         
-%         idx=y+s.*(1:order);
-%         p_joint_y=accumarray(chunked(:,idx),1)./l;
-%         p_joint_y=permute(p_joint_y,[ndims(p_joint_y)+1 1:ndims(p_joint_y)]);
-%         p_cond_y=p_joint_yy./p_joint_y;
-%         
-%         te=p_joint.*log2(p_cond_xy./p_cond_y);
-%         te=sum(te(:),'omitnan');
-%     end
 
     function updateBar(~)
         waitbar(p/N, f);
