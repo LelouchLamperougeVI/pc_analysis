79b8c57c8707165be30f6ac964628295bf7e6fb5
LelouchLamperougeVI <33172704+LelouchLamperougeVI@users.noreply.github.com>
2018-07-23 16:18:57 -0600

    adssasadsadsa

--- template matching/plot_tm.m
+++ template matching/plot_tm.m
@@ -12,18 +12,21 @@ colormap(black);
 
 figure;
 ax1=subplot(4,1,1:2);
-imagesc(deconv(:,order)');
-colormap(black);
+imagesc(fast_smooth(zscore(deconv(:,order)),2)');
+colormap(ax1,black);
 c=colorbar; c.Label.String='dF/F';
 
 ax2=subplot(4,1,3);
 imagesc(analysis.C');
-colormap hot
+colormap(ax2,hot);
 c=colorbar; c.Label.String='Spearman''s Rho';
 
 ax3=subplot(4,1,4);
 imagesc(analysis.C_pval');
-colormap bone
+colormap(ax3,bone);
 c=colorbar; c.Label.String='prctile';
+[x,y]=find(analysis.C_pval>.95);
+hold on
+plot(x,y,'r*');
 
 linkaxes([ax1 ax2 ax3], 'x');
