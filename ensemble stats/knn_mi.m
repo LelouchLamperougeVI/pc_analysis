a24f022e810f547b6f42ac3196b520e55e9cacfe
LelouchLamperougeVI <33172704+LelouchLamperougeVI@users.noreply.github.com>
2018-07-29 23:00:58 -0600

    asadds

---
+++ ensemble stats/knn_mi.m
@@ -0,0 +1,31 @@
+function I=knn_mi(deconv,k)
+% Implementation of k-nearest neighbor based MI estimator
+
+if nargin<2
+    k=1;
+end
+
+I=zeros(size(deconv,2));
+for i=1:size(deconv,2)
+    parfor j=i+1:size(deconv,2)
+        A=deconv(:,[i j]);
+        A(sum(A,2)==0,:)=[];
+        d=knnsearch(A,A,'k',k+1);
+        d=d(:,end);
+        d=A(d,:); %each row's nn
+        dist=abs(d-A);
+%         dist=max(dist,[],2);
+        d=abs(permute(A,[3 2 1])-A);
+        n=sum(d<=dist,3)-1;
+        
+        I(i,j)=psi(k)-1/k-mean(sum(psi(n),2))+psi(size(deconv,1));
+%         I(i,j)=psi(k)-mean(sum(psi(n+1),2))+psi(size(A,1));
+    end
+    i
+end
+
+for i=1:size(deconv,2)
+    for j=i+1:size(deconv,2)
+        I(j,i)=I(i,j);
+    end
+end
