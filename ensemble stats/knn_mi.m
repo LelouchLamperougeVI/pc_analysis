d63670a3aefff82d284ea5984488bb8f8a95144e
LelouchLamperougeVI <33172704+LelouchLamperougeVI@users.noreply.github.com>
2018-08-13 11:05:08 -0600

    qweqweq

--- ensemble stats/knn_mi.m
+++ ensemble stats/knn_mi.m
@@ -5,19 +5,19 @@ if nargin<2
     k=1;
 end
 
-% [~,~,deconv]=par_sort(deconv);
+% [~,~,deconv]=par_sort(deconv); %rank-order data
 
 I=zeros(size(deconv,2));
 for i=1:size(deconv,2)
     parfor j=i+1:size(deconv,2)
         A=deconv(:,[i j]);
-%         A(sum(A,2)==0,:)=[];
-        A=unique(A,'rows');
+        A(sum(A,2)==0,:)=[];
+%         A=unique(A,'rows');
         d=knnsearch(A,A,'k',k+1);
         d=d(:,end);
         d=A(d,:); %each row's k-nn
 %         dist=abs(d-A);
-        dist=abs(max(d,[],2)-A);
+        dist=max(abs(d-A),[],2);
         d=abs(permute(A,[3 2 1])-A);
         n=sum(d<dist,3)-1;
         
